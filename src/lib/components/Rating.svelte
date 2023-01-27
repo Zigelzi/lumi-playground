8acce837e76f737efc21e110fca700456abafb21
zigelzi <miika.a.savela@gmail.com>
2023-01-27 18:03:51 +0200

    Add the rating selection styles

--- src/lib/components/Rating.svelte
+++ src/lib/components/Rating.svelte
@@ -1,10 +1,11 @@
 <script>
+	let rating = 1;
 	let isCompleted = false;
 </script>
 
 <div>
 	{#if !isCompleted}
-		<div class="card">
+		<div class="feedback-form">
 			<span class="bg-circle-gray">
 				<svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"
 					><path
@@ -13,24 +14,89 @@
 					/></svg
 				>
 			</span>
-			<h4 class="card-title">How did we do?</h4>
-			<p>
-				Please let us know how we did with your support request. All feedback is appreciated to help
-				us improve our offering!
-			</p>
-			<ol class="rating">
-				<li class="bg-circle-gray"><span>1</span></li>
-				<li class="bg-circle-gray"><span>2</span></li>
-				<li class="bg-circle-gray"><span>3</span></li>
-				<li class="bg-circle-gray"><span>4</span></li>
-				<li class="bg-circle-gray"><span>5</span></li>
-			</ol>
-			<button>Submit</button>
+			<form action="">
+				<fieldset>
+					<legend class="feedback-form-title">How did we do?</legend>
+					<p>
+						Please let us know how we did with your support request. All feedback is appreciated to
+						help us improve our offering!
+					</p>
+					<ol class="rating">
+						<li>
+							<label class="radio-label" for="rating_1">
+								<input
+									class="radio-input"
+									bind:group={rating}
+									value="1"
+									type="radio"
+									name="rating"
+									id="rating_1"
+								/>
+								<span class="radio-value-text">1</span>
+							</label>
+						</li>
+						<li>
+							<label class="radio-label" for="rating_2">
+								<input
+									class="radio-input"
+									bind:group={rating}
+									value="2"
+									type="radio"
+									name="rating"
+									id="rating_2"
+								/>
+								<span class="radio-value-text">2</span>
+							</label>
+						</li>
+						<li>
+							<label class="radio-label" for="rating_3">
+								<input
+									class="radio-input"
+									bind:group={rating}
+									value="3"
+									type="radio"
+									name="rating"
+									id="rating_3"
+								/>
+								<span class="radio-value-text">3</span>
+							</label>
+						</li>
+						<li>
+							<label class="radio-label" for="rating_4">
+								<input
+									class="radio-input"
+									bind:group={rating}
+									value="4"
+									type="radio"
+									name="rating"
+									id="rating_4"
+								/>
+								<span class="radio-value-text">4</span>
+							</label>
+						</li>
+						<li>
+							<label class="radio-label" for="rating_5">
+								<input
+									class="radio-input"
+									bind:group={rating}
+									value="5"
+									type="radio"
+									name="rating"
+									id="rating_5"
+								/>
+								<span class="radio-value-text">5</span>
+							</label>
+						</li>
+					</ol>
+				</fieldset>
+				<button>Submit</button>
+				<p>You selected {rating}</p>
+			</form>
 		</div>
 	{:else}
-		<div class="card">
+		<div class="feedback-form">
 			<p>You selected 1 out of 5</p>
-			<p class="card-title">Thank you!</p>
+			<p class="feedback-form-title">Thank you!</p>
 			<p>
 				We appreciate you taking the time to give a rating. If you ever need more support, don’t
 				hesitate to get in touch!
@@ -47,7 +113,7 @@
 		--blue-700: hsl(213, 19%, 18%);
 		--blue-900: hsl(216, 12%, 8%);
 	}
-	.card {
+	.feedback-form {
 		background-color: var(--blue-700);
 		color: var(--gray-200);
 		padding: var(--p-m);
@@ -60,30 +126,69 @@
 		margin: var(--mg-l) 0;
 	}
 
-	.rating li {
-		line-height: 1;
+	.radio-label {
+		position: relative;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		width: 50px;
+		height: 50px;
+		cursor: pointer;
 	}
 
-	.bg-circle-gray {
-		position: relative;
+	.radio-label:hover > .radio-value-text::after {
+		background-color: var(--orange);
 	}
 
-	.bg-circle-gray::after {
+	.radio-input {
+		/* Visually hide radio button but keep it accessible for screen readers */
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		opacity: 0;
+	}
+
+	.radio-input:checked + .radio-value-text,
+	.radio-label:hover > .radio-value-text {
+		color: var(--white);
+	}
+
+	.radio-input:checked + .radio-value-text::after,
+	.radio-label:hover > .radio-value-text::after {
+		opacity: 0.8;
+	}
+
+	.radio-value-text {
+		color: var(--gray-200);
+		z-index: 1;
+	}
+
+	.radio-value-text::after {
 		content: '';
 		position: absolute;
-		opacity: 0.2;
-		background-color: var(--gray-500);
-		height: 50px;
-		width: 50px;
-		border-radius: 100%;
-		/* top: -50%; */
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		border-radius: 50%;
+		background-color: var(--gray-200);
+		z-index: -1;
+		opacity: 0.4;
 	}
 
-	.card-title {
+	.radio-value-text,
+	.radio-value-text::after {
+		transition: background-color 0.2s, color 0.1s, opacity 0.2s;
+	}
+
+	.feedback-form-title {
 		color: var(--white);
 		font-size: var(--fs-600);
+	}
+
+	fieldset {
+		border: none;
 	}
 </style>
